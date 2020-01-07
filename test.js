193576dde53a7e315f2d94e5d2293b2ac8dfffa9
Pro.MohamedGalal <ProMohamedGalal@users.noreply.github.com>
2020-01-07 13:34:04 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+/*jslint browser:true, evil: true */
+/*jslint sloppy:true, white:true, browser:true, regexp: true */
+/* eslint-env browser */
+/* eslint-env es6 */
+/* eslint-disable no-console */
+/* eslint-disable */
+/*global
+const, alert, confirm,  console, Debug, opera, prompt, WSH ,switch, case, console, $, continue, clearInterval, clearTimeout, document, event, FileReader, FormData, history, localStorage, location, name, navigator, screen, sessionStorage, setInterval, setTimeout, Storage, URL, window, XMLHttpRequest, emit, getRow, isArray, log, provides, registerType, require, send, start, sum, toJSON, Buffer, clearImmediate, clearInterval, clearTimeout, console, exports, module, process, require, setImmediate, setInterval, setTimeout, URL, URLSearchParams,
+*/
+/* eslint-disable no-console */
+/*jslint plusplus: true */
+
+
+
+/*$(document).ready(function(){
+
+    $("button").click(function(){
+        $("input").attr("class","ginput");
+        $("a").attr("class","ginput");
+        $("div").text($("a").attr("href"));
+    });
+    $("a").click(function(){
+        var z = $("input").val();
+        $("div").text(z);
+    });
+
+});*/
+$('.container').ripples({
+    dropRadius: 5,
+    resolution: 900
+});
+
+
+
+
+
+
+
+
+
+
+
